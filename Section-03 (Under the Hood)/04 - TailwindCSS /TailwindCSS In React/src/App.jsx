c34bd9dff99fb25f3a703ffb0a34a91b149321f9
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-02-18 06:49:51 +0530

    Adding new files

---
+++ Section-03 (Under the Hood)/04 - TailwindCSS /TailwindCSS In React/src/App.jsx
@@ -0,0 +1,23 @@
+// import './App.css'
+// // import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <nav className="float-right mt-6">
+          <ul>
+            <li className="inline m-4 p-4 bg-slate-500 cursor-pointer rounded">Home</li>
+            <li className="inline m-4 p-4 bg-slate-500 cursor-pointer rounded">About</li>
+            <li className="inline m-4 p-4 bg-slate-500 cursor-pointer rounded">Services</li>
+            <li className="inline m-4 p-4 bg-slate-500 cursor-pointer rounded">Carrer</li>
+            <li className="inline m-4 p-4 bg-slate-500 cursor-pointer rounded">Sign Up</li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  )
+}
+
+export default App
